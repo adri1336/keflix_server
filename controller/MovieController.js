22d83a1a99a6327bd55695c1e4d36aef9daa8e43
adri1samp <adri1pawn@gmail.com>
2020-05-11 18:01:55 +0100

    Funcion getMovies

--- controller/MovieController.js
+++ controller/MovieController.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
 const { Movie } = require("../config/db");
 const { Genre } = require("../config/db");
+const ProfileLibraryMovieController = require("../controller/ProfileLibraryMovieController");
+const { Sequelize, Op } = require("sequelize");
+const fs = require("fs");
 
 const create = async (body) => {
     return await Movie.create(body);
@@ -26,8 +30,86 @@ const destroy = async (where) => {
     });
 };
 
+const getMovies = async (options) => {
+    const where = {
+        adult: {
+            [Op.or]: options.include_adult ? [true, false] : [false]
+        },
+        [Op.all]: options.year ? Sequelize.literal("YEAR(release_date) = " + options.year) : Sequelize.literal(""),
+        published: {
+            [Op.or]: options.include_no_published ? [true, false] : [true]
+        }
+    };
+
+    let order = [["libraryMovieId", "DESC"]];
+    if(options.sort_by) {
+        order = [options.sort_by.split(".")];
+    }
+
+    const limit = options.limit || 50;
+    const offset = options.page ? (options.limit * (options.page - 1)) : 0;
+    const movies = await getAll(where, order, limit, offset);
+
+    //Genres Filter
+    let final = [];
+    const with_genres = options.with_genres ? options.with_genres.toString().split(",") : [];
+    const without_genres = options.without_genres ? options.without_genres.toString().split(",") : [];
+    if(with_genres.length > 0 || without_genres.length > 0) {
+        movies.map(movie => {
+            let add = with_genres.length == 0 ? true : false;
+            for(let i = 0; i < movie.genres.length; i ++) {
+                const genre = movie.genres[i];
+                if(with_genres.some(id => genre.id.toString() == id)) {
+                    add = true;
+                }
+                if(without_genres.some(id => genre.id.toString() == id)) {
+                    add = false;
+                    break;
+                }   
+            }
+            if(add) {
+                final.push(movie);
+            }
+        });
+    }
+    else {
+        final = movies;
+    }
+
+    //Profile Info
+    if(options.profile_id) {
+        const profileId = options.profile_id;
+        for(let i = 0; i < final.length; i ++) {
+            const movie = final[i];
+            const profileInfo = await ProfileLibraryMovieController.get({
+                libraryMovieId: movie.libraryMovieId,
+                profileId: profileId
+            });
+            if(profileInfo) {
+                final[i].dataValues.profileInfo = profileInfo.dataValues;
+            }   
+        }
+    }
+
+    //mediaInfo
+    for(let i = 0; i < final.length; i ++) {
+        const movie = final[i];
+        const mediaInfo = {
+            trailer: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/trailer.mp4"),
+            video: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/video.mp4"),
+            poster: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/poster.png"),
+            backdrop: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/backdrop.png"),
+            logo: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/logo.png")
+        };
+        final[i].dataValues.mediaInfo = mediaInfo;
+    }
+
+    return final;
+}
+
 module.exports = {
     create,
     getAll,
-    destroy
+    destroy,
+    getMovies
 };
--- routes/Movie.js
+++ routes/Movie.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
-const path = require("path");
 const fs = require("fs");
-const { Sequelize, Op } = require("sequelize");
 const AccountController = require("../controller/AccountController");
 const MovieController = require("../controller/MovieController");
-const ProfileLibraryMovieController = require("../controller/ProfileLibraryMovieController");
 
 const verifyToken = (token, callback) => {
     if(!token) res.sendStatus(403);
@@ -153,82 +150,18 @@ router.use(middlewareRouter);
 
 router.post("/discover", async (req, res) => {
     try {
-        const where = {
-            adult: {
-                [Op.or]: req.body.include_adult ? [true, false] : [false]
-            },
-            [Op.all]: req.body.year ? Sequelize.literal("YEAR(release_date) = " + req.body.year) : Sequelize.literal(""),
-            published: {
-                [Op.or]: req.body.include_no_published ? [true, false] : [true]
-            }
-        };
-        console.log("where: ", where);
-
-        let order = [["libraryMovieId", "DESC"]];
-        if(req.body.sort_by) {
-            order = [req.body.sort_by.split(".")];
-        }
-        console.log("order: ", order);
-
-        const limit = req.body.limit || 50;
-        const offset = req.body.page ? (req.body.limit * (req.body.page - 1)) : 0;
-        const movies = await MovieController.getAll(where, order, limit, offset);
-
-        //Genres Filter
-        let final = [];
-        const with_genres = req.body.with_genres ? req.body.with_genres.toString().split(",") : [];
-        const without_genres = req.body.without_genres ? req.body.without_genres.toString().split(",") : [];
-        if(with_genres.length > 0 || without_genres.length > 0) {
-            movies.map(movie => {
-                let add = with_genres.length == 0 ? true : false;
-                for(let i = 0; i < movie.genres.length; i ++) {
-                    const genre = movie.genres[i];
-                    if(with_genres.some(id => genre.id.toString() == id)) {
-                        add = true;
-                    }
-                    if(without_genres.some(id => genre.id.toString() == id)) {
-                        add = false;
-                        break;
-                    }   
-                }
-                if(add) {
-                    final.push(movie);
-                }
-            });
-        }
-        else {
-            final = movies;
-        }
-
-        //Profile Info
-        if(req.body.profile_id) {
-            const profileId = req.body.profile_id;
-            for(let i = 0; i < final.length; i ++) {
-                const movie = final[i];
-                const profileInfo = await ProfileLibraryMovieController.get({
-                    libraryMovieId: movie.libraryMovieId,
-                    profileId: profileId
-                });
-                if(profileInfo) {
-                    final[i].dataValues.profileInfo = profileInfo.dataValues;
-                }   
-            }
-        }
-
-        //mediaInfo
-        for(let i = 0; i < final.length; i ++) {
-            const movie = final[i];
-            const mediaInfo = {
-                trailer: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/trailer.mp4"),
-                video: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/video.mp4"),
-                poster: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/poster.png"),
-                backdrop: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/backdrop.png"),
-                logo: fs.existsSync(process.env.MEDIA_MOVIES_PATH + movie.id + "/logo.png")
-            };
-            final[i].dataValues.mediaInfo = mediaInfo;
-        }
-
-        res.json(final);
+        const movies = await MovieController.getMovies({
+            include_adult: req.body.include_adult,
+            year: req.body.year,
+            include_no_published: req.body.include_no_published,
+            sort_by: req.body.sort_by,
+            limit: req.body.limit,
+            page: req.body.page,
+            with_genres: req.body.with_genres,
+            without_genres: req.body.without_genres,
+            profile_id: req.body.profile_id
+        });
+        res.json(movies);
     }
     catch(error) {
         res.status(400).json(error);
